d8775e0bf037e3edd504ef35af925edfc9ed3d87
SeanWiig <seanwiig@gmail.com>
2019-04-18 16:29:29 -0400

    flesh out User.java

--- User.java
+++ User.java
@@ -1,3 +1,31 @@
 public class User {
+    String userID;
+    String firstName;
+    String lastName;
+    String role;
+    String program;
 
+    public User(String newUserID, String newFirstName, String newLastName, String newRole, String newProgram) {
+        userID = newUserID;
+        firstName = newFirstName;
+        lastName = newLastName;
+        role = newRole;
+        program = newProgram;
+    }
+
+    String getUserID() {
+        return userID;
+    }
+    String getFirstName() {
+        return firstName;
+    }
+    String getLastName() {
+        return lastName;
+    }
+    String getRole() {
+        return role;
+    }
+    String getProgram() {
+        return program;
+    }
 }
