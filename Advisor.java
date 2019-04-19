09d6cee0c5de61fc4c1aa8c902534ac2af45f668
BennyRiot <benjaminjritz@gmail.com>
2019-04-19 18:05:04 -0400

    added advisor class

---
+++ Advisor.java
@@ -0,0 +1,17 @@
+public class Advisor {
+    String program;
+    String firstName;
+    String lastName;
+
+    String getProgram() {
+        return program;
+    }  // Ending bracket for method getProgram
+
+    String getFirstName() {
+        return firstName;
+    }  // Ending bracket for method getFirstName
+
+    String getLastName() {
+        return lastName;
+    }  // Ending bracket for method getLastName
+}
