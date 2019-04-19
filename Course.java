50cdd3b5365d5e3626a1a84b29988a5f5dc04984
BennyRiot <benjaminjritz@gmail.com>
2019-04-19 17:23:47 -0400

    populated class Course

--- Course.java
+++ Course.java
@@ -1,3 +1,18 @@
 public class Course {
+    String name;
+    String id;
+    String numCredits;
+
+    String getName() {
+        return name;
+    }  // Ending bracket for method getName
+
+    String getId() {
+        return id;
+    }  // Ending bracekt for method getId
+
+    String getNumCredits() {
+        return numCredits;
+    }  // Ending braceket for method getNumCredits
     
-}
+}  // Ending brackets for class Course
