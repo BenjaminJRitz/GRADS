b97be25490790500daa66fb8113ea21575e559ca
BennyRiot <benjaminjritz@gmail.com>
2019-04-19 18:02:37 -0400

    added major class

---
+++ Major.java
@@ -0,0 +1,17 @@
+public class Major {
+    String major;
+    String semester;
+    String year;
+
+    String getMajor() {
+        return major;
+    }  // Ending bracket for method getmajor
+
+    String getSemesterBegan() {
+        return semester;
+    }  // Ending bracket for method getSemesterBegan
+
+    String getYearBegan() {
+        return year;
+    }  // Ending bracket for method getYearBegan
+}  // Ending bracket for class Major
--- StudentRecord.java
+++ StudentRecord.java
@@ -1,3 +1,21 @@
 public class StudentRecord {
-    
+    //student:
+        String studentID;
+        String networkId;
+        String firstName;
+        String lastName;
+        
+        //perhaps major should be its own class
+        String major;
+            String termBegan;
+            String semester;
+            String year;
+        //perhaps advisor should be its own class
+        Advisor advisor;
+            //String program;
+            //String firstName;
+            //String lastName;
+        CourseTaken[] coursesTaken;
+        String[] notes;
+
 }
