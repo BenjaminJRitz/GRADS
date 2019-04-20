0e68d5235b8d8d5a0912f61c7b84ea6f41644426
BennyRiot <benjaminjritz@gmail.com>
2019-04-19 23:10:22 -0400

    more classes

---
+++ CompInfo.java
@@ -0,0 +1,11 @@
+public class CompInfo {
+    //2 OF
+    String[] CIS_CAROLINA_CORE_ARP = {"CSCE145", "MATH122", "MATH141"};
+    //2 OF
+    String[] CIS_CAROLINA_CORE_SCI = {"BIOL101", "BIOL101L", "BIOL102", "BIOL102L", "BIOL120", "BIOL120L", 
+                                "BIOL243", "BIOL242L", "BIOL244", "BIOL244L", "BIOL270", "BIOL270L", "CHEM111", 
+                                "CHEM111L", "CHEM112", "CHEM112L", "GEOL215", "GEOL215L", "MSCI210", "MSCI210L", 
+                                "MCSI215", "MCSI215L", "PHYS201", "PHYS201L", "PHYS202", "PHYS202L", "PHYS211", 
+                                "PHYS211L", "PHYS212", "PHYS212L"};
+    
+}
---
+++ CompSci.java
@@ -0,0 +1,30 @@
+public class CompSci {
+    //0-3 OR 4-7
+    String[] CSCE_CAROLINA_CORE_SCI = {"CHEM111", "CHEM111L", "CHEM112", "CHEM112L",
+                                    "PHYS211", "PHYS211L", "PHYS212", "PHYS212L"};
+    // ALL OF
+    String[] CSCE_CAROLINA_CORE_ARP = {"MATH141", "MATH142"};
+    //ALL OF
+    String[] CSCE_GEN_ED_REQUIRED = {"MATH241", "MATH374", "MATH344", "MATH344L", "STAT509"};
+    //ONE OF
+    String[] CSCE_GEN_ED_LAB = {"ANTH161", "ASTR101", "BIO101", "BIO101L", "BIO110", "CHEM111", "CHEM111L",
+                            "CHEM141", "ENVR101", "ENVR101L", "ENVR200", "GEOG201", "GEOG202", "GEOL101",
+                            "GEOL103", "GEOL201", "GEOL215", "GEOL215L", "GEOL302", "MSCI101", "MSCI102",
+                            "MSCI215", "MSCI215L", "PHYS211", "PHYS211L"};
+    //THREE OF
+    String[] CSCE_GEN_ED_LIBARTS = {" AERO401", "AERO402", "ANTH101", "ARMY401", "ARMY402", "ARTE101", "ARTE260", 
+                            "ARTH105", "ARTS103", "CHIN103", "CLAS220", "CPLT150", "CRJU101", "DANC101", "ECON123", 
+                            "ENGL270", "FAMS180", "FREN109", "GEOG103", "GERM190", "HIST101", "ITAL101", "JAPA121", 
+                            "LASP201", "LATN109", "LING300", "MART110", "MUSC110", "NAVY401", "NAVY402", "PHIL101", 
+                            "PHIL112", "POLI101", "PORT121", "PSYC101", "RELG101", "RUSS121", "SOCY101", "SOST101", 
+                            "SPAN109", "THEA170", "WGST112"};
+    //all of
+    String[] CSCE_LOWER_DIV_COMPUTING = {"CSCE145", "CSCE146", "CSCE190", "CSCE211", "CSCE212", "CSCE215", "CSCE240", "CSCE247"};
+    //ALL OF
+    String[] CSCE_REQUIRED = {"CSCE311", "CSCE330", "CSCE350", "CSCE355", "CSCE416", "CSCE490", "CSCE492"};
+    //THREE OF ** 2 OF CSCE500 - CSCE699 ** 
+    String[] CSCE_ELECTIVE = {"CSCE317", "CSCE500", "CSCE699"};
+    //9 CLASSES FROM APPLICATION AREA
+    String[] APPLICATION_AREA = {"", ""};
+
+}
