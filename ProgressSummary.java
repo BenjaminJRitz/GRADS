0dcf06b6d0cd33bad850542ec03f9bc790ec0eb8
BennyRiot <benjaminjritz@gmail.com>
2019-04-21 00:01:16 -0400

    fixed progress summary

--- ProgressSummary.java
+++ ProgressSummary.java
@@ -1,33 +1,37 @@
 public class ProgressSummary {
-    StudentRecord studentRecord;
-    Double percentageComplete;
+    StudentRecord record;
 
-    ProgressSummary(StudentRecord record) {
-        this.studentRecord = record;
-    }
+    ProgressSummary(StudentRecord pRecord) {
+        this.record = pRecord;
+    }  // Ending bracket for constructor
 
     /** 
      * This method will have to compute the GPA for the passed in student record.
      * This can get the grade for each class taken from the coursesTaken array.
     */
-    double getGPA() {
-        return 0;
-    }
+    float getGPA() {
+        return record.getGPA();
+    }  // Ending bracket for method getGPA
 
-    String getStudentName() {
-        return studentRecord.getStudent().getFirstName() +
-         " " + studentRecord.getStudent().getLastName();
+    Student getStudent() {
+        return record.getStudent();
     }  // Ending bracket for method getStudentName
 
-    String getAdvisorName() {
-        return studentRecord.getAdvisor().getFirstName() +
-         " " + studentRecord.getAdvisor().getLastName();
+    String getMajor() {
+        return record.getMajor();
+    }  // Ending bracket for method getMajor
+
+    Advisor getAdvisor() {
+        return record.getAdvisor();
     }  // Ending bracket for method getAdvisorName
 
-    String getSemesterBegan() {
-        return studentRecord.getTermBegan().getSemesterBegan() +
-         " " + studentRecord.getTermBegan().getYearBegan();
+    TermBegan getTermBegan() {
+        return record.getTermBegan();
     }  // Ending bracket for method getSemesterBegan
+
+    RequirementEvaluated[] getRequirements() {
+        return null;
+    }  // Ending bracket for method getRequirements
 
 
 }
